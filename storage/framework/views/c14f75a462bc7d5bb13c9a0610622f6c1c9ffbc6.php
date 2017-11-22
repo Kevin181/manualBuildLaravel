039ba9d9b485fc838bdb11395fb767eeb95f674f
Kevin181 <yuan364205famile@gmail.com>
2017-11-22 11:31:03 +0800

    Delete c14f75a462bc7d5bb13c9a0610622f6c1c9ffbc6.php

--- storage/framework/views/c14f75a462bc7d5bb13c9a0610622f6c1c9ffbc6.php
+++
@@ -1,5 +0,0 @@
-<h3>Show student messages in the view</h3>
-jjj
-student id: <?php echo e($data['id']); ?>;<br/>
-student name: <?php echo e($data['name']); ?>;<br/>
-student age: <?php echo e($data['age']); ?>;<br/>
